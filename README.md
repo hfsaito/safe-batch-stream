d994428d68b713865c93a24e88a1661d29a1fbd6
Hiroshi Farias Saito <hiroshi.fs94@gmail.com>
2020-03-12 00:38:07 -0300

    Setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # safe-batch-stream
+Safely creates an array of elements inside a stream. It prevents [backpressuring in streams](https://nodejs.org/es/docs/guides/backpressuring-in-streams/) by waiting the drain after reaching a limit of batches built
+
+## Example
+```javascript
+// Default batch size 1
+// Default batch limit 1
+const safeBatchStream = new SafeBatchStream();
+
+stream
+  .pipe(safeBatchStream)
+  .pipe();
+```
---
+++ src/safe-batch-stream.ts
@@ -0,0 +1,47 @@
+import { Transform } from 'stream';
+
+export class SafeBatchStream extends Transform {
+  private batch: any[] = [];
+
+  /**
+   * Safely creates an array of elements inside a stream
+   * @param {number} [batchSize=1] Number of elements per batch
+   * @param {number} [batchLimit=1] Limit of how many batches are made to prevent backpressure
+   */
+  constructor(
+    private readonly batchSize: number = 1,
+    private readonly batchLimit: number = 1,
+  ) {
+    super({
+      readableObjectMode: true,
+      writableObjectMode: true
+    });
+  }
+
+  _transform = (
+    chunk: any,
+    _encoding: string,
+    callback: (error?: Error) => void
+  ) => {
+    this.batch.push(chunk);
+    if (this.batch.length >= this.batchSize) {
+      this.push(this.batch);
+      this.batch = [];
+    }
+    if (this.readableLength >= this.batchLimit) {
+      this.once('drain', callback);
+    } else {
+      callback();
+    }
+  }
+
+  _flush = (
+    callback: (error?: Error) => void
+  ) => {
+    if (this.batch.length) {
+      this.push(this.batch);
+      this.batch = [];
+    }
+    callback();
+  }
+}
